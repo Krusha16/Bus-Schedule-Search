a8489f862282f9cdd1942ee15f9dfb3ba159bf3a
Krusha Patel <krushampatel@yahoo.com>
2020-12-18 19:58:27 -0600

    perform monifacation of css and js files, transpiling of js files, clean dist task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
-const {src, dest, series, parallel} = require('gulp');
+const {src, dest, series} = require('gulp');
+const terser = require('gulp-terser');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const csso = require('gulp-csso');
+const del = require('del');
+
+function cleanTask() {
+  return del('dist');
+}
 
 function htmlTask(){
   return src('src/*html')
@@ -7,15 +17,23 @@ function htmlTask(){
 
 function scriptsTask(){
   return src('src/js/*.js')
+  .pipe(babel({presets: ["@babel/env"]}))
+  .pipe(sourcemaps.init())
+  .pipe(terser())
+  .pipe(sourcemaps.write())
   .pipe(dest('dist/js'))
 }
 
 function stylesTask(){
   return src(['src/css/*'])
+  .pipe(sourcemaps.init())
+  .pipe(autoprefixer())
+  .pipe(csso())
+  .pipe(sourcemaps.write())
   .pipe(dest('dist/css'))
 }
 
 exports.html = htmlTask;
 exports.scripts = scriptsTask;
 exports.styles = stylesTask;
-exports.default = series(htmlTask, scriptsTask, stylesTask);
+exports.default = series(cleanTask, htmlTask, scriptsTask, stylesTask);
